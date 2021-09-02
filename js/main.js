8916dfc5e06fcc83f8dbdc84e7d5b1f63b9615ad
bjitdas <bjitdas10@gmail.com>
2021-09-02 15:52:20 +0600

    third commit

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 const searchBooks = () => {
     const searchField = document.getElementById('input-field');
     const searchText = searchField.value;
-    // console.log(searchText)
+    
 ///////// clear input value//////// 
-    searchField.value = ''
+    searchField.value = '';
+
+    
 ///////////loadData///////////
     const url = `http://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
@@ -14,11 +16,25 @@ const searchBooks = () => {
 // display result
 const displaySearchResult = (books) => {
    const searchContainer = document.getElementById('search-container');
+
 /////////handle search result quantity////// 
    const resultQuantity = document.getElementById('result-quantity');
+
+        /////clear search quantity 
+   resultQuantity.textContent = '';
+
    const p = document.createElement('p');
-   p.innerText = `${books.length} results found.`
-   resultQuantity.appendChild(p);
+   console.log(books.length)
+    if(books.length === 0){
+        p.innerText = `No result found.` 
+    }
+    else{
+        p.innerText = `${books.length} results found.`
+    }
+        resultQuantity.appendChild(p);
+
+ //////////clear search result///////
+   searchContainer.textContent = '';
 
 ////////handle book detail///////// 
     books.forEach(book => {
@@ -26,18 +42,22 @@ const displaySearchResult = (books) => {
         const imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         const div = document.createElement('div');
         div.classList.add('col');
+        
+   
+
         div.innerHTML = `
             <div class="card">
-            <img src="${imgUrl}" class="card-img-top" alt="...">
+            <img src="${imgUrl}" class="card-img-top w-100" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${book.title}</h5>
-                    <p class="card-text">Author-name: ${book?.author_name}</p>
-                    <p class="card-text">First-publish: ${book?.first_publish_year}</p>
+                    <p class="card-text">Author: ${book?.author_name}</p>
+                    <p class="card-text">First-published: ${book?.first_publish_year}</p>
                 </div>
             </div>
         `
     searchContainer.appendChild(div);
     })
 
+    
 }    
  
